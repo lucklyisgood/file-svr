56c970bea0d0d716d1441399a10e776cf4dc62e2
vker <weidonghao@zuoyebang.com>
2024-03-14 17:32:23 +0800

    first commit

---
+++ main.ts
@@ -0,0 +1,31 @@
+import { Hono } from "https://deno.land/x/hono@v4.1.0/mod.ts"
+import { serveStatic, cors, compress, logger } from "https://deno.land/x/hono@v4.1.0/middleware.ts"
+
+const app = new Hono()
+
+app.use(cors())
+app.use(compress())
+app.use(logger())
+
+app.get('/', (c) => c.text('Hello Deno!'))
+
+app.use('/static/uploads/*', serveStatic({ root: './' }))
+
+app.post('/upload', async (c) => {
+  const body = await c.req.parseBody()
+  const dist_path = body['path']
+  const file = body['file']
+  const file_data = await file.stream()
+
+  await Deno.mkdir(`./static/uploads/${dist_path}`, { recursive: true })
+
+  const filePath = `./static/uploads/${dist_path}/${file.name}`
+
+  console.log(dist_path, file, filePath)
+
+  await Deno.writeFile(filePath, file_data)
+
+  return c.text("succ")
+});
+
+Deno.serve(app.fetch)
